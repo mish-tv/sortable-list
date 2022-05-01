f4e470c2a7c42596b65a9b4aae5b1b1aec54a2b4
Koji Murata <malt.koji@gmail.com>
2022-05-01 14:42:56 +0900

    currentDraggedIndex

--- src/sortable-list.tsx
+++ src/sortable-list.tsx
@@ -9,12 +9,36 @@ type Props<Row extends HTMLElement, I extends Item> = Readonly<{
 }>;
 
 export const SortableList = <Row extends HTMLElement, I extends Item>(props: Props<Row, I>) => {
-  const onDrag = React.useCallback((item: I, y: number) => {
-    console.info(item.id, y);
-  }, []);
+  const idToIndex = React.useMemo(
+    () => props.items.reduce((previous, current, i) => ({ ...previous, [current.id]: i }), {} as Record<React.Key, number>),
+    [props.items],
+  );
+  const offsetTopValues = React.useRef<Record<React.Key, number>>({});
+  const currentDraggedIndex = React.useRef<number>();
 
-  const updateOffsetTop = React.useCallback((item: I, top: number) => console.info(item.id, top), []);
-  // refs.current = props.items.map(() => React.createRef());
+  const onDrag = React.useCallback(
+    (item: I, y: number) => {
+      const index = idToIndex[item.id];
+      const draggedIndex = currentDraggedIndex.current ?? index;
+      const draggingOffset = offsetTopValues.current[item.id] + y;
+      const upOffset = offsetTopValues.current[props.items[draggedIndex - 1].id];
+      const downOffset = offsetTopValues.current[props.items[draggedIndex + 1].id];
+
+      if (draggingOffset < upOffset) {
+        currentDraggedIndex.current = draggedIndex - 1;
+      }
+      if (draggingOffset > downOffset) {
+        currentDraggedIndex.current = draggedIndex + 1;
+      }
+
+      console.info(currentDraggedIndex.current);
+    },
+    [idToIndex],
+  );
+
+  const updateOffsetTop = React.useCallback((item: I, top: number) => {
+    offsetTopValues.current = { ...offsetTopValues.current, [item.id]: top };
+  }, []);
 
   const rows = React.useMemo(
     () =>
