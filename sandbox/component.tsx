a58b7584622f534d96a3c046f1976dae7530fbbf
Koji Murata <malt.koji@gmail.com>
2022-05-05 18:37:17 +0900

    sandbox

---
+++ sandbox/component.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { RowCreator, SortableList } from "@mish-tv/sortable-list";
+
+type Item = { id: number; body: string };
+
+export const Component = (props: { initialIds: number[]; items: Record<number, Item> }) => {
+  const [ids, setIds] = React.useState(props.initialIds);
+
+  const row: RowCreator<HTMLLIElement, number> = React.useCallback(
+    (id, rowAttributes, handleAttributes, options) => {
+      const item = props.items[id];
+      let className = "row";
+      if (options.isDragging) className += " dragging";
+
+      return (
+        <li className={className} {...rowAttributes}>
+          <button type="button" {...handleAttributes}>
+            ⣿
+          </button>
+          <span>{item.body}</span>
+        </li>
+      );
+    },
+    [props.items],
+  );
+
+  return (
+    <ul className="list">
+      <SortableList ids={ids} setIds={setIds} row={row} scrollBoundaryTop={50} scrollBoundaryBottom={100} />
+    </ul>
+  );
+};
--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -1,45 +1,16 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { RowCreator, SortableList } from "@mish-tv/sortable-list";
+import React from "react";
+import ReactDOM from "react-dom";
 
-type Item = { id: number; body: string; height: number; marginTop: number };
+import { Component } from "./component";
+
 const bodies = ["foo", "bar", "baz"];
-const items: Item[] = Array(50)
+const items = Array(50)
   .fill(0)
-  .map((_, i) => ({ id: i, body: bodies[i % 3], height: 20 + (i % 3) * 20, marginTop: 5 + (i % 3) * 5 }));
+  .map((_, i) => ({ id: i, body: `${i} ${bodies[i % 3]}` }));
+const ids = Array(50)
+  .fill(0)
+  .map((_, i) => i);
 
-const App = () => {
-  const [ids, setIds] = React.useState(
-    Array(50)
-      .fill(0)
-      .map((_, i) => i),
-  );
-
-  const row: RowCreator<HTMLLIElement, number> = React.useCallback((id, rowAttributes, handleAttributes, options) => {
-    const item = items[id];
-    let className = "row";
-    if (options.isDragging) className += " dragging";
-
-    return (
-      <li
-        className={className}
-        {...rowAttributes}
-        style={{ ...rowAttributes.style, height: item.height, marginTop: item.marginTop }}
-      >
-        <button type="button" {...handleAttributes}>
-          ⣿
-        </button>
-        <span>{item.id}</span>
-        <span>{item.body}</span>
-      </li>
-    );
-  }, []);
-
-  return (
-    <ul className="list">
-      <SortableList ids={ids} setIds={setIds} row={row} scrollBoundaryTop={50} scrollBoundaryBottom={100} />
-    </ul>
-  );
-};
+export const App = () => <Component initialIds={ids} items={items} />;
 
 ReactDOM.render(<App />, document.getElementById("root"));
