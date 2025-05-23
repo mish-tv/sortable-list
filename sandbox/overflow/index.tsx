6ca825aa13f9a6ded37fcdb5e5a99d9139d087c7
Koji Murata <k@malt03.com>
2025-05-23 14:53:43 +0900

    sandbox

--- sandbox/overflow/index.tsx
+++ sandbox/overflow/index.tsx
@@ -1,20 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import { Component } from "./component";
 
 const bodies = ["foo", "bar", "baz"];
 const items = Array(50)
-  .fill(0)
-  .map((_, i) => ({ id: i, body: `${i} ${bodies[i % 3]}` }));
+	.fill(0)
+	.map((_, i) => ({ id: i, body: `${i} ${bodies[i % 3]}` }));
 const ids = Array(50)
-  .fill(0)
-  .map((_, i) => i);
+	.fill(0)
+	.map((_, i) => i);
 
 export const App = () => (
-  <div className="wrapper">
-    <Component initialIds={ids} items={items} />
-  </div>
+	<div className="wrapper">
+		<Component initialIds={ids} items={items} />
+	</div>
 );
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+if (container) {
+	createRoot(container).render(<App />);
+}
